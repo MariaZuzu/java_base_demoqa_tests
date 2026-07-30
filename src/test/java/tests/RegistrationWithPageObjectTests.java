4a2d67488dbb89cc7d34aa205bda40581e95ff06
Maria Zueva <zuuma10@gmail.com>
2026-07-30 14:11:28 +0300

    Added more 1 test for allure notifications

--- src/test/java/tests/RegistrationWithPageObjectTests.java
+++ src/test/java/tests/RegistrationWithPageObjectTests.java
@@ -54,57 +54,57 @@ public class RegistrationWithPageObjectTests extends TestBase {
         });
     }
 
-//    @Test
-//    void successfulFillAllFormTest_old() {
-//
-//        open("/automation-practice-form");
-//        $("[id=firstName111]").setValue(firstName);
-//        $("[id=lastName]").setValue(lastName);
-//        $("[id=userEmail]").setValue(userEmail);
-//        $("#genterWrapper").$(byText(genderWrapper)).click();
-//        $("[id=userNumber]").setValue(userNumber);
-//
-//
-//        $("[id=dateOfBirthInput]").click();
-//        $(".react-datepicker__month-select").selectOption(month);
-//        $(".react-datepicker__year-select").selectOption(year);
-//        $(".react-datepicker__month").$(byText(day)).click();
-//
-//
-//        $("[id=subjectsInput]").setValue(subjectsInput).pressEnter();
-//        $("#hobbiesWrapper").$(byText(hobbiesWrapper)).click();
-//
-//
-//        $("#uploadPicture").uploadFromClasspath(uploadPicture);
-//        $("#uploadPicture").shouldHave(value(uploadPicture));
-//
-//        $("[id=currentAddress]").setValue(currentAddress);
-//
-//        $("[id=state]").click();
-//        $(byText(state)).click();
-//
-//        $("[id=city]").click();
-//        $(byText(city)).click();
-//
-//        $("[id=submit]").click();
-//
-//
-//        $(".modal-content").shouldBe(com.codeborne.selenide.Condition.visible);
-//
-//        // Проверка конкретных полей в таблице результатов
-//        $(".modal-body table").shouldHave(text(userName));
-//        $(".modal-body table").shouldHave(text(userEmail));
-//        $(".modal-body table").shouldHave(text(genderWrapper));
-//        $(".modal-body table").shouldHave(text(userNumber));
-//        $(".modal-body table").shouldHave(text(userBirthDay));
-//        $(".modal-body table").shouldHave(text(subjectsInput));
-//        $(".modal-body table").shouldHave(text(hobbiesWrapper));
-//        $(".modal-body table").shouldHave(text(uploadPicture));
-//        $(".modal-body table").shouldHave(text(currentAddress));
-//        $(".modal-body table").shouldHave(text(stateAndCity));
-//
-//        $("#closeLargeModal").click();
-//
-//    }
+    @Test
+    void successfulFillAllFormTest_old() {
+
+        open("/automation-practice-form");
+        $("[id=firstName111]").setValue(firstName);
+        $("[id=lastName]").setValue(lastName);
+        $("[id=userEmail]").setValue(userEmail);
+        $("#genterWrapper").$(byText(genderWrapper)).click();
+        $("[id=userNumber]").setValue(userNumber);
+
+
+        $("[id=dateOfBirthInput]").click();
+        $(".react-datepicker__month-select").selectOption(month);
+        $(".react-datepicker__year-select").selectOption(year);
+        $(".react-datepicker__month").$(byText(day)).click();
+
+
+        $("[id=subjectsInput]").setValue(subjectsInput).pressEnter();
+        $("#hobbiesWrapper").$(byText(hobbiesWrapper)).click();
+
+
+        $("#uploadPicture").uploadFromClasspath(uploadPicture);
+        $("#uploadPicture").shouldHave(value(uploadPicture));
+
+        $("[id=currentAddress]").setValue(currentAddress);
+
+        $("[id=state]").click();
+        $(byText(state)).click();
+
+        $("[id=city]").click();
+        $(byText(city)).click();
+
+        $("[id=submit]").click();
+
+
+        $(".modal-content").shouldBe(com.codeborne.selenide.Condition.visible);
+
+        // Проверка конкретных полей в таблице результатов
+        $(".modal-body table").shouldHave(text(userName));
+        $(".modal-body table").shouldHave(text(userEmail));
+        $(".modal-body table").shouldHave(text(genderWrapper));
+        $(".modal-body table").shouldHave(text(userNumber));
+        $(".modal-body table").shouldHave(text(userBirthDay));
+        $(".modal-body table").shouldHave(text(subjectsInput));
+        $(".modal-body table").shouldHave(text(hobbiesWrapper));
+        $(".modal-body table").shouldHave(text(uploadPicture));
+        $(".modal-body table").shouldHave(text(currentAddress));
+        $(".modal-body table").shouldHave(text(stateAndCity));
+
+        $("#closeLargeModal").click();
+
+    }
 
 }
